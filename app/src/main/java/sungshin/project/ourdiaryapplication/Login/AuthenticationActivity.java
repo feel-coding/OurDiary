44ad7b38ea9e3b520293647fec7ea674a7d92fa0
YeoKyeong <yeokeong99@gmail.com>
2020-05-16 22:39:47 +0900

    완료 버튼을 누르면 화면이동하기

--- app/src/main/java/sungshin/project/ourdiaryapplication/Login/AuthenticationActivity.java
+++ app/src/main/java/sungshin/project/ourdiaryapplication/Login/AuthenticationActivity.java
@@ -2,15 +2,29 @@ package sungshin.project.ourdiaryapplication.Login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import sungshin.project.ourdiaryapplication.R;
+import sungshin.project.ourdiaryapplication.main.MainActivity;
 
 public class AuthenticationActivity extends AppCompatActivity {
 
+    Button completeBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
+
+        completeBtn = findViewById(R.id.completeBtn);
+
+        completeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AuthenticationActivity.this.finish();
+            }
+        });
     }
 }
