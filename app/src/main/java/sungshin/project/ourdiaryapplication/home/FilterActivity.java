5a3c09076ff93ae34a59280ed7c717aefc6a2356
feel-coding <khe628@naver.com>
2020-05-18 02:09:48 +0900

    날짜별, 친구별 필터 추가

--- app/src/main/java/sungshin/project/ourdiaryapplication/home/FilterActivity.java
+++ app/src/main/java/sungshin/project/ourdiaryapplication/home/FilterActivity.java
@@ -1,16 +1,66 @@
 package sungshin.project.ourdiaryapplication.home;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.ListView;
 
 import sungshin.project.ourdiaryapplication.R;
 
 public class FilterActivity extends AppCompatActivity {
 
+    ListView listView;
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
+        toolbar = findViewById(R.id.filter_toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true); //커스터마이징 하기 위해 필요
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
+        actionBar.setHomeAsUpIndicator(R.drawable.close_black);
+        listView = findViewById(R.id.friendSelectLv);
+        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+        listView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+            @Override
+            public void onItemCheckedStateChanged(ActionMode actionMode, int i, long l, boolean b) {
+
+            }
+
+            @Override
+            public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+                MenuInflater inflater = getMenuInflater();
+                inflater.inflate(R.menu.contextual, menu);
+                toolbar.setVisibility(View.GONE);
+                return true;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
+                return false;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode actionMode) {
+
+            }
+        });
     }
 }
--- app/src/main/java/sungshin/project/ourdiaryapplication/home/fragments/HomeAllFragment.java
+++ app/src/main/java/sungshin/project/ourdiaryapplication/home/fragments/HomeAllFragment.java
@@ -1,8 +1,12 @@
 package sungshin.project.ourdiaryapplication.home.fragments;
 
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,12 +14,15 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 
 import sungshin.project.ourdiaryapplication.R;
+import sungshin.project.ourdiaryapplication.home.FilterActivity;
 
 
 public class HomeAllFragment extends Fragment {
@@ -24,6 +31,11 @@ public class HomeAllFragment extends Fragment {
     ArrayList<Diary> diaryList;
     DiaryAdapter adapter;
     ImageButton filterBtn;
+    ArrayAdapter<String> yearAndMonthAdapter;
+    String[] yearAndMonth;
+    Spinner spinner;
+    Context mContext;
+    Activity activity;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -31,6 +43,18 @@ public class HomeAllFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_home_all, container, false);
         recyclerView = v.findViewById(R.id.diaryBook);
         filterBtn = v.findViewById(R.id.filterBtn);
+        spinner = v.findViewById(R.id.yearAndMonthSpinner);
+        yearAndMonth = new String[]{"2020년 3월", "2020년 4월", "2020년 5월"};
+        yearAndMonthAdapter = new ArrayAdapter<>(activity, android.R.layout.simple_spinner_dropdown_item, yearAndMonth);
+        spinner.setAdapter(yearAndMonthAdapter);
+
+        filterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), FilterActivity.class);
+                startActivity(intent);
+            }
+        });
         diaryList = new ArrayList<>();
         diaryList.add(new Diary("김효은", "어버이날", "2020-05-08","어버이날이라 카네이션과 케이크를 준비했다. 부모님이 좋아하셨다."));
         diaryList.add(new Diary("김효은", "홍대 간 날", "2020-05-07","오늘은 오랜만에 홍대를 갔다. 패션피플들이 정말 많았다. 맛있는 것도 먹고 정말 재미있게 놀았다."));
@@ -47,4 +71,11 @@ public class HomeAllFragment extends Fragment {
         return v;
     }
 
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mContext = context;
+        if (context instanceof Activity)
+            activity = (Activity) context;
+    }
 }
