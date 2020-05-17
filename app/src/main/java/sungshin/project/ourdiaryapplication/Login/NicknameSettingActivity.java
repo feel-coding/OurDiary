2c516e6199697f4057ce5e613f6fa985d4bed2c1
feel-coding <khe628@naver.com>
2020-05-17 11:58:53 +0900

    닉네임 받는 부분 추가

---
+++ app/src/main/java/sungshin/project/ourdiaryapplication/Login/NicknameSettingActivity.java
@@ -0,0 +1,14 @@
+package sungshin.project.ourdiaryapplication.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NicknameSettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nickname_setting);
+    }
+}
