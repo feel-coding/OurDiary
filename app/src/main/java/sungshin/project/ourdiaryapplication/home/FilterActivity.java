64d88e11db0f360c910b526d8a0f3696f0896d24
feel-coding <khe628@naver.com>
2020-05-18 03:38:55 +0900

    필터 적용 액티비티 뒤로가기 동작하도록, 에딧텍스트 커서 위치 조정

--- app/src/main/java/sungshin/project/ourdiaryapplication/home/FilterActivity.java
+++ app/src/main/java/sungshin/project/ourdiaryapplication/home/FilterActivity.java
@@ -1,5 +1,6 @@
 package sungshin.project.ourdiaryapplication.home;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -62,5 +63,14 @@ public class FilterActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
