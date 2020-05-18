f912b21f5f4a34ce1997ddd68621731ad4d91fca
feel-coding <khe628@naver.com>
2020-05-18 13:43:48 +0900

    닉네임 변경 alert dialog 추가

--- app/src/main/java/sungshin/project/ourdiaryapplication/mypage/MyPageFragment.java
+++ app/src/main/java/sungshin/project/ourdiaryapplication/mypage/MyPageFragment.java
@@ -2,14 +2,17 @@ package sungshin.project.ourdiaryapplication.mypage;
 
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -25,18 +28,42 @@ public class MyPageFragment extends Fragment {
     Activity activity;
     private Context mContext;
     Button toPasswordSetting;
-
+    Button changeNicknameBtn;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_my_page, container, false);
         toPasswordSetting = v.findViewById(R.id.btn_mypage_app_lock);
+        changeNicknameBtn = v.findViewById(R.id.btn_mypage_change_nick);
         toPasswordSetting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(mContext, LockSettingActivity.class);
                 startActivity(i);
+            }
+        });
+        changeNicknameBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final EditText nicknameEdit = new EditText(v.getContext());
+                final AlertDialog.Builder nicknameEditDialog = new AlertDialog.Builder(v.getContext());
+
+                nicknameEditDialog.setTitle("닉네임 변경")
+                        .setMessage("변경할 닉네임을 입력하세요").setView(nicknameEdit)
+                        .setPositiveButton("변경", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                String nickname = nicknameEdit.getText().toString();
+                            }
+                        }).setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                AlertDialog alertDialog = nicknameEditDialog.create();
+                alertDialog.show();
             }
         });
         return v;
