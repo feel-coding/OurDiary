5eb641cafa9d8afd14be0e4040bde1b13171c80c
feel-coding <khe628@naver.com>
2020-06-14 02:46:47 +0900

    그룹 만들기 화면 추가

---
+++ app/src/main/java/sungshin/project/ourdiaryapplication/friendlist/GroupRequestActivity.java
@@ -0,0 +1,14 @@
+package sungshin.project.ourdiaryapplication.friendlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GroupRequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_request);
+    }
+}
---
+++ app/src/main/java/sungshin/project/ourdiaryapplication/home/ImageShowActivity.java
@@ -0,0 +1,14 @@
+package sungshin.project.ourdiaryapplication.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImageShowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_show);
+    }
+}
